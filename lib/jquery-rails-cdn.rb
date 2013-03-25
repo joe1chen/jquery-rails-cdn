7ecc84d1b3ff0bdbd162196b636aace4594e9161
Joseph Chen <joe1chen@hotmail.com>
2013-03-25 11:04:11 -0700

    Add jquery-migrate support (probably necessary if you're upgrading to jquery 1.9+).

--- lib/jquery-rails-cdn.rb
+++ lib/jquery-rails-cdn.rb
@@ -4,6 +4,7 @@ require 'jquery-rails-cdn/version'
 module Jquery::Rails::Cdn
   module ActionViewExtensions
     JQUERY_VERSION = Jquery::Rails::JQUERY_VERSION
+    JQUERY_MIGRATE_VERSION = "1.1.1"
     JQUERY_UI_VERSION = Jquery::Rails::JQUERY_UI_VERSION
     OFFLINE = (Rails.env.development? or Rails.env.test?)
     JQUERY_URL = {
@@ -12,6 +13,13 @@ module Jquery::Rails::Cdn
       :jquery             => "http://code.jquery.com/jquery-#{JQUERY_VERSION}.min.js",
       :yandex             => "//yandex.st/jquery/#{JQUERY_VERSION}/jquery.min.js",
       :cloudflare         => "//cdnjs.cloudflare.com/ajax/libs/jquery/#{JQUERY_VERSION}/jquery.min.js"
+    }
+    JQUERY_MIGRATE_URL = {
+      #:google             => "//ajax.googleapis.com/ajax/libs/jquery-migrate/#{JQUERY_MIGRATE_VERSION}/jquery.min.js",
+      #:microsoft          => "//ajax.aspnetcdn.com/ajax/jQuery/jquery-migrate-#{JQUERY_MIGRATE_VERSION}.min.js",
+      :jquery             => "http://code.jquery.com/jquery-migrate-#{JQUERY_MIGRATE_VERSION}.min.js" #,
+      #:yandex             => "//yandex.st/jquery-migrate/#{JQUERY_MIGRATE_VERSION}/jquery.min.js",
+      #:cloudflare         => "//cdnjs.cloudflare.com/ajax/libs/jquery-migrate/#{JQUERY_MIGRATE_VERSION}/jquery.min.js"
     }
     JQUERY_UI_URL = {
       :google             => "//ajax.googleapis.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
@@ -23,6 +31,11 @@ module Jquery::Rails::Cdn
 
     def jquery_url(name, options = {})
       JQUERY_URL[name]
+    end
+
+    def jquery_migrate_url(name, options = {})
+      # Use jquery CDN for jquery-migrate if it does not exist in requested CDN.
+      JQUERY_MIGRATE_URL[name] || JQUERY_MIGRATE_URL[:jquery]
     end
 
     def jquery_ui_url(name, options = {})
@@ -37,11 +50,19 @@ module Jquery::Rails::Cdn
       ].join("\n").html_safe
     end
 
+    def jquery_migrate_include_tag(name, options = {})
+      return javascript_include_tag(:"jquery-migrate-#{JQUERY_MIGRATE_VERSION}.min") if OFFLINE and !options[:force]
+
+      [ javascript_include_tag(jquery_migrate_url(name, options)),
+        javascript_tag("window.jQuery || document.write(unescape('#{javascript_include_tag(:"jquery-migrate-#{JQUERY_MIGRATE_VERSION}.min").gsub('<','%3C')}'))")
+      ].join("\n").html_safe
+    end
+
     def jquery_ui_include_tag(name, options = {})
-      return javascript_include_tag("jquery-ui") if OFFLINE and !options[:force]
+      return javascript_include_tag(:"jquery-ui") if OFFLINE and !options[:force]
 
       [ javascript_include_tag(jquery_ui_url(name, options)),
-        javascript_tag("(window.jQuery && window.jQuery.ui) || document.write(unescape('#{javascript_include_tag("jquery-ui").gsub('<','%3C')}'))")
+        javascript_tag("(window.jQuery && window.jQuery.ui) || document.write(unescape('#{javascript_include_tag(:"jquery-ui").gsub('<','%3C')}'))")
       ].join("\n").html_safe
     end
   end
