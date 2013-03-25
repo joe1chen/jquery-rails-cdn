6e417cc45d0b71e743e5ed6147f67c819c6df4ad
Joseph Chen <joe1chen@hotmail.com>
2013-03-25 10:34:00 -0700

    Add cloudflare jqueryui js.

--- lib/jquery-rails-cdn.rb
+++ lib/jquery-rails-cdn.rb
@@ -17,7 +17,8 @@ module Jquery::Rails::Cdn
       :google             => "//ajax.googleapis.com/ajax/libs/jqueryui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
       :microsoft          => "//ajax.aspnetcdn.com/ajax/jquery.ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
       :jquery             => "http://code.jquery.com/ui/#{JQUERY_UI_VERSION}/jquery-ui.js",
-      :yandex             => "//yandex.st/jquery-ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js"
+      :yandex             => "//yandex.st/jquery-ui/#{JQUERY_UI_VERSION}/jquery-ui.min.js",
+      :cloudflare         => "//cdnjs.cloudflare.com/ajax/libs/jqueryui/#{JQUERY_VERSION}/jquery-ui.min.js"
     }
 
     def jquery_url(name, options = {})
